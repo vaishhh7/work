706f4527c682b3a8eaa050847310235488c894b5
vaish7github <vaishnavitiwari968@gmail.com>
2026-02-06 15:57:09 +0530

    app with pages made, libs with shared made

---
+++ apps/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@org/apps" />
+    </div>
+  );
+}
+
+export default App;
---
+++ libs/src/lib/libs.tsx
@@ -0,0 +1,9 @@
+export function OrgLibs() {
+  return (
+    <div>
+      <h1>Welcome to OrgLibs!</h1>
+    </div>
+  );
+}
+
+export default OrgLibs;
